809b51fbd7a14c95ffdcef4aecf95dc44ff09eb5
Alena <alena.karpets02@mail.ru>
2025-09-09 23:29:03 +0300

    добавление 2х файлов

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,3 +1,13 @@
+"""from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.random_quote, name='random_quote'),
+    path('add/', views.add_quote, name='add_quote'),
+    path('popular/', views.popular_quotes, name='popular_quotes'),
+    path('like/<int:quote_id>/', views.like_quote, name='like_quote'),
+    path('dislike/<int:quote_id>/', views.dislike_quote, name='dislike_quote'),
+]"""
 from django.urls import path
 from . import views
 
@@ -5,6 +15,8 @@ urlpatterns = [
     path('', views.random_quote, name='random_quote'),
     path('add/', views.add_quote, name='add_quote'),
     path('popular/', views.popular_quotes, name='popular_quotes'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('edit/<int:quote_id>/', views.edit_quote, name='edit_quote'),
     path('like/<int:quote_id>/', views.like_quote, name='like_quote'),
     path('dislike/<int:quote_id>/', views.dislike_quote, name='dislike_quote'),
 ]
